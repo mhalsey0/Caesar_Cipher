a45b636e68aeb85c350a5c9afb63c3bc7df978ff
Mike Halsey <mhalsey0@gmail.com>
2024-09-24 10:40:59 -0400

    first commit

---
+++ Caesar_Cipher/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+      
+      Console.WriteLine("Write your message here:");
+      string message = Console.ReadLine();
+      string messageLower = message.ToLower();
+      char[] secretMessage = messageLower.ToCharArray();
+
+      char[] encryptedMessage = new char[secretMessage.Length];
+
+      for (int i = 0; i < secretMessage.Length; i++)
+      {
+        char secretChar = secretMessage[i];
+        if (Array.IndexOf(alphabet, secretChar) >= 0)
+        {
+          int secretCharIndex = Array.IndexOf(alphabet, secretChar);
+          int encryptedCharIndex = (secretCharIndex + 3) % alphabet.Length;
+          char encryptedChar = alphabet[encryptedCharIndex];
+          encryptedMessage[i] = encryptedChar;
+        }
+        else
+        {
+          continue;
+        }
+      }
+
+      Console.WriteLine(String.Join("", encryptedMessage));
+
+
+
+    }
+  }
+}
